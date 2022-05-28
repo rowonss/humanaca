c3668a95b5ae012e1b0c15a89a2b9e8f32ec039a
rowonss <95suskjg@naver.com>
2022-05-28 20:09:16 +0900

    20220528

---
+++ mypppage/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+import App from './App';
+import SimpleSlider from './App';
+ 
+ReactDOM.render(<SimpleSlider />,document.getElementById('main'));
